e37825c7050adf65b9303891bb03925f59b09cbc
VladimirGoncharov-GG <vgoncharov@gridgain.com>
2020-06-07 00:18:00 +0300

    Odometry

--- src/odrive.cpp
+++ src/odrive.cpp
@@ -204,6 +204,9 @@ void sendOdometry(tf::TransformBroadcaster odom_broadcaster, ros::Publisher odom
 
 
 void publishJointState(ros::Publisher joint_state_pub){
+    joint_states.position = pos;
+    joint_states.velocity = vel;
+    joint_states.effort = eff;
 	joint_states.header.stamp = current_time;
 	joint_state_pub.publish(joint_states);
 }
