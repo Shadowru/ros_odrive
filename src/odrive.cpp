623b0a22b8fc7703a3215db25a25c26744b941df
VladimirGoncharov-GG <vgoncharov@gridgain.com>
2020-06-07 00:03:43 +0300

    Odometry

--- src/odrive.cpp
+++ src/odrive.cpp
@@ -163,7 +163,7 @@ void sendOdometry(tf::TransformBroadcaster odom_broadcaster, ros::Publisher odom
     //first, we'll publish the transform over tf
     geometry_msgs::TransformStamped odom_trans;
     odom_trans.header.stamp = current_time;
-    odom_trans.header.frame_id = "ros_odrive/odom";
+    odom_trans.header.frame_id = "odom";
     odom_trans.child_frame_id = "base_link";//footprint";
 
     odom_trans.transform.translation.x = robot_x_pos;
@@ -177,7 +177,7 @@ void sendOdometry(tf::TransformBroadcaster odom_broadcaster, ros::Publisher odom
     //next, we'll publish the odometry message over ROS
     nav_msgs::Odometry odom;
     odom.header.stamp = current_time;
-    odom.header.frame_id = "ros_odrive/odom";
+    odom.header.frame_id = "odom";
 
     //set the position
     odom.pose.pose.position.x = robot_x_pos;
