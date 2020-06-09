3afe50eb0d69a30c4de69fd91c28c8264b04f3de
VladimirGoncharov-GG <vgoncharov@gridgain.com>
2020-06-09 23:00:00 +0300

    WebUI

--- src/odrive.cpp
+++ src/odrive.cpp
@@ -492,7 +492,7 @@ int main(int argc, char **argv) {
     }
 
     //setPID(0.05, 0.1);
-    setPID(0.018, 0.4);
+    setPID(0.03, 0.4);
 
     current_time = ros::Time::now();
     last_time = ros::Time::now();
